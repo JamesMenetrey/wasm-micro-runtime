0ab215aac0c34f8728277e7f0efe310c2e1bf031
Jämes Ménétrey <james.menetrey@unine.ch>
2022-09-13 13:31:48 +0000

    Relocate SGX IPFS files into the SGX folder.

--- core/iwasm/libraries/libc-wasi/libc_wasi.cmake
+++ core/iwasm/libraries/libc-wasi/libc_wasi.cmake
@@ -6,8 +6,7 @@ set (LIBC_WASI_DIR ${CMAKE_CURRENT_LIST_DIR})
 add_definitions (-DWASM_ENABLE_LIBC_WASI=1)
 
 include_directories(${LIBC_WASI_DIR}/sandboxed-system-primitives/include
-                    ${LIBC_WASI_DIR}/sandboxed-system-primitives/src
-                    ${LIBC_WASI_DIR}/sgx/include)
+                    ${LIBC_WASI_DIR}/sandboxed-system-primitives/src)
 
 file (GLOB_RECURSE source_all ${LIBC_WASI_DIR}/*.c )
 
